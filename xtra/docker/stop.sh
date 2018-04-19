6969ad2fc61284d10d119b2bdff91564d8b20d24
HyechurnJang <hyjang@cisco.com>
2018-04-19 19:11:32 +0900

    update

--- xtra/docker/stop.sh
+++ xtra/docker/stop.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+echo "Stop ADUN Web GUI"
 docker rm -f -v adun_webgui
-docker rm -f -v adun_engine
-docker rm -f -v adun_db
+echo ""
 
+echo "Stop ADUN Engine"
+docker rm -f -v adun_engine
+echo ""
+
+echo "Stop ADUN Database"
+docker rm -f -v adun_db
+echo ""
+echo "Completed"
+echo ""
